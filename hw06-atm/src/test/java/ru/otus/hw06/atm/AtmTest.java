a92d5d92911b9daff85ea604c030dcbca469cb27
Ruslan <ruslanraiz@gmail.com>
2019-08-17 15:41:40 +0600

    fix test

--- hw06-atm/src/test/java/ru/otus/hw06/atm/AtmTest.java
+++ hw06-atm/src/test/java/ru/otus/hw06/atm/AtmTest.java
@@ -5,13 +5,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ru.otus.hw06.atm.cassette.Cassette;
 import ru.otus.hw06.atm.cassette.FaceValue;
-import ru.otus.hw06.atm.exception.NotEnoughAmountException;
-import ru.otus.hw06.atm.exception.UnsupportedFaceValueException;
 
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayName("Test ATM")
 class AtmTest {
@@ -29,7 +29,7 @@ class AtmTest {
     }
 
     @Test
-    void getCassettes_isSorted_ASC() {
+    void getCassettes_isSorted_DESC() {
         int last = 100000;
         for (Cassette cassette : atm.getCassettes()) {
             assertThat(cassette.getFaceValue().getValue()).isLessThan(last);
@@ -39,8 +39,10 @@ class AtmTest {
 
     @Test
     void deposit() {
-        assertDoesNotThrow( () -> this.atm.deposit(FaceValue.FIFTY, 5));
-        assertDoesNotThrow( () -> this.atm.deposit(FaceValue.HUNDRED, 100));
+        assertDoesNotThrow( () -> {
+            atm.deposit(FaceValue.FIFTY, 5);
+            atm.deposit(FaceValue.HUNDRED, 100);
+        });
     }
 
     @Test
@@ -64,26 +66,21 @@ class AtmTest {
 
     @Test
     void withdraw() {
+        AtomicReference<Map<FaceValue, Integer>> withdraw = new AtomicReference<>();
+
         assertDoesNotThrow(() -> {
             atm.deposit(FaceValue.FIFTY, 10);
             atm.deposit(FaceValue.HUNDRED, 10);
             atm.deposit(FaceValue.FIVE_HUNDRED, 10);
             atm.deposit(FaceValue.THOUSAND, 10);
+
+            withdraw.set(atm.withdraw(10650));
         });
 
 
-        Map<FaceValue, Integer> withdraw = null;
-
-        try {
-            withdraw = atm.withdraw(10650);
-        } catch (NotEnoughAmountException | UnsupportedFaceValueException e) {
-            System.out.println(e.getMessage());
-        }
-
         assertEquals(5850, atm.balance());
-        assertNotNull(withdraw);
 
-        withdraw.forEach((FaceValue faceValue, Integer count) -> {
+        withdraw.get().forEach((FaceValue faceValue, Integer count) -> {
                              if (faceValue.getValue() == 1000) {
                                  assertEquals(10, count.intValue());
                              }
@@ -97,6 +94,6 @@ class AtmTest {
                                  assertEquals(1, count.intValue());
                              }
                          }
-                        );
+                              );
     }
 }
