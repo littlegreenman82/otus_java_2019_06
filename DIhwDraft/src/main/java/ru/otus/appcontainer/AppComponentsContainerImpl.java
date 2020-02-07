1af0c9ee036c9de1fea036e2afeaf5dc666e1a07
Ruslan <ruslan.zhantekin@gmail.com>
2020-02-07 14:35:18 +0600

    code review fix

--- DIhwDraft/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
+++ DIhwDraft/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
@@ -5,88 +5,87 @@ import ru.otus.appcontainer.api.AppComponentsContainer;
 import ru.otus.appcontainer.api.AppComponentsContainerConfig;
 import ru.otus.appcontainer.api.ComponentCreationException;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class AppComponentsContainerImpl implements AppComponentsContainer {
-  /**
-   * Судя по всему, считаем что order == 0, указывает на отсутствие зависимостей
-   */
-  private static final int ROOT_ORDER = 0;
-  
-  private final List<Object> appComponents = new ArrayList<>();
-  private final Map<String, Object> appComponentsByName = new HashMap<>();
-  
-  public AppComponentsContainerImpl(Class<?> initialConfigClass) throws Exception {
-    processConfig(initialConfigClass);
-  }
-  
-  private void processConfig(Class<?> configClass) throws Exception {
-    checkConfigClass(configClass);
-    final var methods = configClass.getDeclaredMethods();
-    final var instance = configClass.getConstructors()[0].newInstance();
     
-    Arrays.sort(methods, Comparator.comparingInt(method -> method.getAnnotation(AppComponent.class).order()));
+    private final List<Object> appComponents = new ArrayList<>();
+    private final Map<String, Object> appComponentsByName = new HashMap<>();
     
-    for (Method method : methods) {
-      if (method.isAnnotationPresent(AppComponent.class)) {
-        final var annotation = method.getAnnotation(AppComponent.class);
-        
-        List<Object> dependencies = new ArrayList<>();
-        if (annotation.order() != ROOT_ORDER)
-          dependencies = this.getDependencies(method);
-        
-        final var invokedObject = method.invoke(instance, dependencies.toArray());
-        
-        if (invokedObject != null) {
-          appComponents.add(invokedObject);
-          appComponentsByName.put(annotation.name(), invokedObject);
+    public AppComponentsContainerImpl(Class<?> initialConfigClass) throws ComponentCreationException {
+        processConfig(initialConfigClass);
+    }
+    
+    private void processConfig(Class<?> configClass) throws ComponentCreationException {
+        try {
+            checkConfigClass(configClass);
+            final var methods = this.getConfigClassMethods(configClass);
+            final Object instance = configClass.getConstructors()[0].newInstance();
+            final var annotatedMethods = Arrays.stream(methods)
+                    .filter(method -> method.isAnnotationPresent(AppComponent.class))
+                    .sorted(Comparator.comparingInt(method -> method.getAnnotation(AppComponent.class).order()))
+                    .collect(Collectors.toList());
+            
+            for (Method method : annotatedMethods) {
+                Object[] dependencies = this.getDependencies(method);
+                final var annotation = method.getAnnotation(AppComponent.class);
+                final var invokedObject = method.invoke(instance, dependencies);
+                
+                if (invokedObject != null) {
+                    appComponents.add(invokedObject);
+                    appComponentsByName.put(annotation.name(), invokedObject);
+                }
+            }
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            throw new ComponentCreationException("Unable create instance of config class");
         }
-      }
     }
-  }
-  
-  @Override
-  public <C> C getAppComponent(Class<C> componentClass) {
-    for (Object appComponent : appComponents) {
-      if (componentClass.isInstance(appComponent)){
+    
+    @Override
+    public <C> C getAppComponent(Class<C> componentClass) {
+        for (Object appComponent : appComponents) {
+            if (componentClass.isInstance(appComponent)) {
+                //noinspection unchecked
+                return (C) appComponent;
+            }
+        }
+        
+        return null;
+    }
+    
+    @Override
+    public <C> C getAppComponent(String componentName) {
         //noinspection unchecked
-        return (C) appComponent;
-      }
+        return (C) appComponentsByName.get(componentName);
     }
     
-    return null;
-  }
-  
-  @Override
-  public <C> C getAppComponent(String componentName) {
-    if (appComponentsByName.containsKey(componentName)) {
-      //noinspection unchecked
-      return (C) appComponentsByName.get(componentName);
+    private Method[] getConfigClassMethods(Class<?> configClass) {
+        return configClass.getDeclaredMethods();
     }
     
-    return null;
-  }
-  
-  private List<Object> getDependencies(Method method) throws ComponentCreationException {
-    final var parameterTypes = method.getParameterTypes();
-    var dependencies = new ArrayList<>();
-    
-    for (Class<?> parameterType : parameterTypes) {
-      final var dependency = this.getAppComponent(parameterType);
-      
-      if (dependency == null)
-        throw new ComponentCreationException("App container doesn't contain object with type " + parameterType);
-      
-      dependencies.add(dependency);
+    private Object[] getDependencies(Method method) throws ComponentCreationException {
+        final var parameterTypes = method.getParameterTypes();
+        var dependencies = new Object[parameterTypes.length];
+        
+        for (int i = 0; i < parameterTypes.length; i++) {
+            final var dependency = this.getAppComponent(parameterTypes[i]);
+            
+            if (dependency == null) {
+                throw new ComponentCreationException("App container doesn't contain object with type " + parameterTypes[i]);
+            }
+            
+            dependencies[i] = dependency;
+        }
+        
+        return dependencies;
     }
     
-    return dependencies;
-  }
-  
-  private void checkConfigClass(Class<?> configClass) {
-    if (!configClass.isAnnotationPresent(AppComponentsContainerConfig.class)) {
-      throw new IllegalArgumentException(String.format("Given class is not config %s", configClass.getName()));
+    private void checkConfigClass(Class<?> configClass) {
+        if (!configClass.isAnnotationPresent(AppComponentsContainerConfig.class)) {
+            throw new IllegalArgumentException(String.format("Given class is not config %s", configClass.getName()));
+        }
     }
-  }
 }
